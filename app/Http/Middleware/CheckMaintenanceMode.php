657e5f3c3775c45909d2b529340998fea5ae02d5
curringas <curro@martinezhidalgo.com>
2025-12-13 22:26:10 +0100

    quitar modo mantenimiento

--- app/Http/Middleware/CheckMaintenanceMode.php
+++ app/Http/Middleware/CheckMaintenanceMode.php
@@ -20,9 +20,9 @@ class CheckMaintenanceMode
         // Puedes usar una variable de entorno también si quieres:
         // $allowedIp = env('MY_DEV_IP', '127.0.0.1');
         // dd($request->ip(), $allowedIps);
-        if (!in_array($request->ip(), $allowedIps)) {
+        /*if (!in_array($request->ip(), $allowedIps)) {
             return response()->view('mantenimiento.maintenance'); // Vista personalizada
-        }
+        }*/
 
         return $next($request);
     }
